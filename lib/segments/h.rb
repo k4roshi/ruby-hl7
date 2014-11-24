5e741fa3b07764073e0b520917e2c1b3321e4d23
michele <michele@moku.io>
2014-11-24 14:05:34 +0100

    H segment

--- lib/segments/h.rb
+++ lib/segments/h.rb
@@ -2,10 +2,17 @@
 require 'ruby-hl7'
 class HL7::Message::Segment::H < HL7::Message::Segment
     weight 0
-    add_field :filler #\^&
-    add_field :unknown_1
-    add_field :unknown_2
-    add_field :name
-    add_field :version
-    add_field :unknown_3
-  end
+    add_field :delimiters #\^&
+    add_field :message_control_id
+    add_field :access_password
+    add_field :sender_name
+    add_field :sender_address
+    add_field :reserved_1
+    add_field :sender_phone
+    add_field :sender_params
+    add_field :receiver_name
+    add_field :comments
+    add_field :processing_id  # P for Production / T for training / D for debugging
+    add_field :version        # eg. LIS2-A2 or ASTM E1394-91
+    add_field :current_timestamp
+end
